c8342f9c9850137ffa28818b3c11aa15adbf6701
Chris Finney <chrisfinney86@gmail.com>
2017-07-01 20:49:11 -0500

    Starting of the project

---
+++ server/index.js
@@ -0,0 +1,8 @@
+//cmf
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var api = require('/api');
+
+var app = require('express');
