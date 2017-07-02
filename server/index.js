205f2695afd20bfb1172543eb474f2fd9f66acee
Chris Finney <chrisfinney86@gmail.com>
2017-07-01 21:05:56 -0500

    pause at 9:05

--- server/index.js
+++ server/index.js
@@ -5,4 +5,12 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var api = require('/api');
 
-var app = require('express');
+var app = express();
+
+var clientPath = path.join(__dirname, "../client");
+app.use(express.static(clientPath));
+app.use(bodyParser.json());
+
+app.use('/api', api);
+
+app.listen(3000);
